726a41ccadb54c1e57b3038e9b69fc4951245bf7
fabiopereirareis <espynho23@gmail.com>
2021-09-27 20:43:39 -0300

    ContactDTO class add

---
+++ back-end/src/main/java/com/portifolio/pizzariappk/dto/ContactDTO.java
@@ -0,0 +1,8 @@
+package com.portifolio.pizzariappk.dto;
+
+public class ContactDTO {
+
+    private Long id_contact;
+    private String number;
+    private String type; // mudar para enum
+}
---
+++ back-end/src/main/java/com/portifolio/pizzariappk/mapper/ContactMapper.java
@@ -0,0 +1,2 @@
+package com.portifolio.pizzariappk.mapper;public class ContactMapper {
+}
