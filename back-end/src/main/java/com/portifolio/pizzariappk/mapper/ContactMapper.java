0617710c0225fb4dc3a15bbd8d2d79a64db47969
fabiopereirareis <espynho23@gmail.com>
2021-09-27 21:01:19 -0300

    add contactMapper

--- back-end/src/main/java/com/portifolio/pizzariappk/mapper/ContactMapper.java
+++ back-end/src/main/java/com/portifolio/pizzariappk/mapper/ContactMapper.java
@@ -1,2 +1,14 @@
-package com.portifolio.pizzariappk.mapper;public class ContactMapper {
+package com.portifolio.pizzariappk.mapper;
+
+import com.portifolio.pizzariappk.dto.ContactDTO;
+import com.portifolio.pizzariappk.entities.Contact;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+@Mapper(componentModel = "spring")
+public interface ContactMapper {
+
+    ContactMapper INSTANCE = Mappers.getMapper(ContactMapper.class);
+    
+    ContactDTO contactToContactDTO(Contact contact);
 }
