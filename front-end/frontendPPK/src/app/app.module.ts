196647a48e5f3dd0b85dafa46e78934710b74c74
fabiopereirareis <espynho23@gmail.com>
2021-09-30 23:53:12 -0300

    add navbar component

--- front-end/frontendPPK/src/app/app.module.ts
+++ front-end/frontendPPK/src/app/app.module.ts
@@ -8,12 +8,14 @@ import { HeaderComponent } from './components/template/header/header.component';
 
 import { MatToolbarModule} from '@angular/material/toolbar';
 import { FooterComponent } from './components/template/footer/footer.component';
+import { NavbarComponent } from './components/template/navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
