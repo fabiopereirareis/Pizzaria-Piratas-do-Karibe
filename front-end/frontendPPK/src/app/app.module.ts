f6a7379f7a5b505a7dcc7860c32fa56f3c679b3f
fabiopereirareis <espynho23@gmail.com>
2021-10-01 20:00:44 -0300

    add home component and product component

--- front-end/frontendPPK/src/app/app.module.ts
+++ front-end/frontendPPK/src/app/app.module.ts
@@ -1,24 +1,34 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+// componentes criados no início da instalação 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HeaderComponent } from './components/template/header/header.component';
 
+// componentes importados
 import { MatToolbarModule} from '@angular/material/toolbar';
-import { FooterComponent } from './components/template/footer/footer.component';
-import { NavbarComponent } from './components/template/navbar/navbar.component';
-
 import {MatSidenavModule} from '@angular/material/sidenav'; 
 import { MatListModule } from '@angular/material/list'
+import { MatCardModule } from '@angular/material/card'
+
+// meus componentes criados no Angular CLI
+import { HeaderComponent } from './components/template/header/header.component';
+import { FooterComponent } from './components/template/footer/footer.component';
+import { NavbarComponent } from './components/template/navbar/navbar.component';
+import { HomeComponent } from './views/home/home.component';
+import { ProductCrudComponent } from './views/product-crud/product-crud.component';
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    NavbarComponent
+    NavbarComponent,
+    HomeComponent,
+    ProductCrudComponent
   ],
   imports: [
     BrowserModule,
@@ -26,7 +36,8 @@ import { MatListModule } from '@angular/material/list'
     BrowserAnimationsModule,
     MatToolbarModule,
     MatSidenavModule,
-    MatListModule
+    MatListModule,
+    MatCardModule
     
   ],
   providers: [],
