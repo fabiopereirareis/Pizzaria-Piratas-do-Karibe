c1c2ce33c3be027dfe8ccead453eb69db4afdc7f
fabiopereirareis <espynho23@gmail.com>
2021-10-03 21:34:14 -0300

    teste10 netilify

--- frontendPPK/src/app/app.module.ts
+++ frontendPPK/src/app/app.module.ts
@@ -7,13 +7,13 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 // componentes importados
-import { MatToolbarModule} from '@angular/material/toolbar';
-import {MatSidenavModule} from '@angular/material/sidenav'; 
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatListModule } from '@angular/material/list';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
-import { HttpClientModule} from '@angular/common/http';
+// import { HttpClientModule } from '@angular/common/http';
 
 // meus componentes criados no Angular CLI
 import { HeaderComponent } from './components/template/header/header.component';
@@ -45,7 +45,7 @@ import { ProductCreateComponent } from './components/product/product-create/prod
     MatCardModule,
     MatButtonModule,
     MatSnackBarModule
-    
+
   ],
   providers: [],
   bootstrap: [AppComponent]
